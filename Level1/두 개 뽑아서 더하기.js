29b22226273a137fa7b60cf6df5c08cbadf491b5
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2022-07-17 16:06:59 +0900

    Set 오호

---
+++ Level1/두 개 뽑아서 더하기.js
@@ -0,0 +1,34 @@
+
+// Set을 사용하면 객체 안에 유이한 값만 존재함 
+// new Set()을 사용하며 결과가 객체로 나오기 때문에
+// 전개연산자로 풀어서 배열에 담아준당 
+// 오호 
+
+function solution(numbers) {
+    let arr = [];
+    for(let i = 0; i < numbers.length; i++) {
+        for(let j = i+1; j < numbers.length; j++) {
+                arr.push(numbers[i]+numbers[j])
+        }
+    }
+    
+    const result = new Set(arr.sort((a,b) => a-b))
+    
+    return [...result]
+}
+
+
+// 처음 풀었던 풀이 
+
+function solution(numbers) {
+    let result = [];
+    for(let i = 0; i < numbers.length; i++) {
+        for(let j = i+1; j < numbers.length; j++) {
+            if(!result.includes(numbers[i]+numbers[j])) {
+                result.push(numbers[i]+numbers[j])
+            }
+        }
+    }
+    
+    return result.sort((a,b) => a-b)
+}
