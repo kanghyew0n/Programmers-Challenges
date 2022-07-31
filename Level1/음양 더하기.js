421face7923d1d0d3cc989c274c799373cc8290c
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2022-07-31 23:10:16 +0900

    으먕

---
+++ Level1/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+    for(let i = 0; i < signs.length; i++) {
+        if(signs[i] === false){
+            absolutes[i] = -absolutes[i];
+        }
+    }
+
+    return absolutes.reduce( (prev, curr) => prev + curr);
+}
