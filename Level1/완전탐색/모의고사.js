e0d3f1b4d9963224065826f7430d62f6982a7218
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2022-08-14 20:53:10 +0900

    다시 풀어보 모의고사

--- Level1/완전탐색/모의고사.js
+++ Level1/완전탐색/모의고사.js
@@ -1,3 +1,32 @@
+// 다시 푼 모의고사 
+// filter에 두번째 인자로 인덱스를 받을 수 있다는 것을 잊지말자 ~!
+
+function solution(answers) {
+    const answer_1 = [1, 2, 3, 4, 5];
+    const answer_2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const answer_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    
+    let scoreArr = [0, 0, 0];
+    const result = []
+
+    scoreArr[0] = answers.filter((el, idx) => el === answer_1[idx % answer_1.length]).length
+    scoreArr[1] = answers.filter((el, idx) => el === answer_2[idx % answer_2.length]).length
+    scoreArr[2] = answers.filter((el, idx) => el === answer_3[idx % answer_3.length]).length
+    
+    for(let i = 0; i < scoreArr.length; i++) {
+        if(scoreArr[i] === Math.max(...scoreArr)) {
+            result.push(i+1);
+        }
+    }
+     return result
+}
+
+
+
+// 첫번째로 풀어보 모의고사 
+// 모의고사 문제가 기존 수포자들의 답보다 길경우를 생각해서 
+// 답변을 받는 함수(answerAll) 만들었당 
+
 function solution(answers) {
     const result = []
     const answer_1 = [1, 2, 3, 4, 5];
